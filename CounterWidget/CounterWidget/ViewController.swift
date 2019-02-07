6511cac299cb329f026003e11e8fb1e629dda0da
Poncho <oaguerrero.95@gmail.com>
2019-02-07 12:37:14 -0600

    Xcode Project Created
    Single View App

---
+++ CounterWidget/CounterWidget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CounterWidget
+//
+//  Created by Oscar Alfonso Guerrero on 2/7/19.
+//  Copyright © 2019 Oscar Alfonso Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
